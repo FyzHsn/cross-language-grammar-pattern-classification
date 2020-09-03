478640487b1833a0433b7c9a34986c9ec4743d31
Faiyaz Hasan <faiyaz.hasan1@gmail.com>
2020-09-03 11:30:37 -0400

    Start data exploration stub

--- grammar_tagging.py
+++ grammar_tagging.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import spacy
 
+from sklearn.feature_extraction.text import \
+    CountVectorizer, \
+    TfidfVectorizer
 from spacy import displacy
 
 
@@ -8,37 +11,30 @@ class GrammarTagger:
     def __init__(self, df=None):
         self.df = df
         self.nlp = spacy.load("en_core_web_sm")
+        self.X = None
 
-    def pos_tags(self, sentence):
-        doc = self.nlp(sentence)
-        for token in doc:
-            print(token.text, token.pos_, token.tag_)
-        displacy.serve(doc, style="dep")
+    def generate_tags(self):
+        tags = []
+        for index, row in self.df.iterrows():
+            doc = self.nlp(row['sentence'])
+            for token in doc:
+                tags += [token.tag_, token.dep_]
+        self.df['tags'] = " ".join(tags)
+
+    def get_vectorize_data(self):
+        vectorizer = CountVectorizer()
+        corpus = self.df['tags'].values
+        X = vectorizer.fit_transform(corpus)
+        tfidf = TfidfVectorizer()
+        X = tfidf.fit_transform(X)
+        return X
 
 
 if __name__ == "__main__":
-    grammer = GrammarTagger()
-    group_1 = ["The class studied",
-               "The students and the teacher read.",
-               "The students sat and read.",
-               "The students and the teacher sat and read."]
-    group_2 = ["The class took a test.",
-               "The class took a test and a quiz."]
-    group_3 = ["The class worked carefully.",
-               "The students sit here.",
-               "The class worked like a team.",
-               "Before school, in the gym, the class worked like a team.",
-               "In the gym, the class worked like a team before school.",
-               "Like a team, the class worked before school in the gym."]
-    group_4 = ["The teacher is Mr. Soto.",
-               "The teachers are Mr. Soto and Ms. Lin"]
-    group_5 = ["The teacher is kind.",
-               "Ms. Kin is kind and helpful."]
-    group_6 = ["The teacher gave the class a test",
-               "Mr. Soto gave Kim and John a test."]
-
-    # for sentence in group_6:
-    #     grammer.pos_tags(sentence)
-    #     print("===========")
-
-    grammer.pos_tags("the teacher gave the class a test")
+    df = pd.read_csv('data/raw_data.csv')
+    df_subset = df[df['language'] == 'eng'].copy()
+    grammar = GrammarTagger(df_subset)
+    grammar.generate_tags()
+    print(grammar.df.head())
+    print(grammar.df.tail())
+    print(grammar.get_vectorize_data())
