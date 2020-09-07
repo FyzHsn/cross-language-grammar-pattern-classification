60b069f7267a8848af814cdee7dd01390d80681d
Faiyaz Hasan <faiyaz.hasan1@gmail.com>
2020-09-07 14:00:20 -0400

    Add methods to create tfidf of POS and synctactic dependency tags

--- grammar_tagging.py
+++ grammar_tagging.py
@@ -1,40 +1,75 @@
+import math
+import numpy as np
 import pandas as pd
 import spacy
 
-from sklearn.feature_extraction.text import \
-    CountVectorizer, \
-    TfidfVectorizer
-from spacy import displacy
+from collections import namedtuple
+
+SentenceData = namedtuple('SentenceData', 'sentence_id language sentence ' 
+                                          'token_freq')
 
 
 class GrammarTagger:
     def __init__(self, df=None):
         self.df = df
+        self.data = None
+        self.token_doc_freq = {}
+        self.token_id = {}
+        self.inverse_token_id = {}
+        self.token_num = 0
         self.nlp = spacy.load("en_core_web_sm")
-        self.X = None
 
-    def generate_tags(self):
-        tags = []
+    def _add_new_token(self, token):
+        self.token_num += 1
+        self.token_id[token] = (self.token_num - 1)
+        self.inverse_token_id[(self.token_num - 1)] = token
+
+    def _update_vocabulary(self, tokens):
+        for token in tokens:
+            if token in self.token_doc_freq:
+                self.token_doc_freq[token] += 1
+            else:
+                self._add_new_token(token)
+                self.token_doc_freq[token] = 1
+
+    def _get_sentence_tokens(self, sentence):
+        sentence = self.nlp(sentence)
+        sentence_token_freq = {}
+        for token in sentence:
+            for tag in [token.tag_, token.dep_]:
+                if tag in sentence_token_freq:
+                    sentence_token_freq[tag] += 1
+                else:
+                    sentence_token_freq[tag] = 1
+        return sentence_token_freq
+
+    def generate_tag_data(self):
+        self.data = []
         for index, row in self.df.iterrows():
-            doc = self.nlp(row['sentence'])
-            for token in doc:
-                tags += [token.tag_, token.dep_]
-        self.df['tags'] = " ".join(tags)
+            token_freq = self._get_sentence_tokens(row['sentence'])
+            self._update_vocabulary(list(token_freq.keys()))
+            self.data.append(SentenceData(sentence_id=row['sentence_id'],
+                                          language=row['language'],
+                                          sentence=row['sentence'],
+                                          token_freq=token_freq))
 
-    def get_vectorize_data(self):
-        vectorizer = CountVectorizer()
-        corpus = self.df['tags'].values
-        X = vectorizer.fit_transform(corpus)
-        tfidf = TfidfVectorizer()
-        X = tfidf.fit_transform(X)
-        return X
+    def _tfidf(self, term_frequency, document_frequency):
+        doc_num = self.data.shape[0]
+        inverse_document_frequency = \
+            math.log((1.0 + doc_num) / (1.0 + document_frequency)) + 1
+        return term_frequency * inverse_document_frequency
+
+    def tfidf_transform(self):
+        tfidf_feat = np.zeros(shape=(df.shape[0], self.token_num))
+        for row_idx, row in enumerate(self.data):
+            for token, term_freq in row.token_freq.items():
+                col_idx = self.token_id[token]
+                doc_freq = self.token_doc_freq[token]
+                tfidf_feat[row_idx][col_idx] = self._tfidf(term_freq, doc_freq)
+        return tfidf_feat
 
 
 if __name__ == "__main__":
     df = pd.read_csv('data/raw_data.csv')
     df_subset = df[df['language'] == 'eng'].copy()
     grammar = GrammarTagger(df_subset)
-    grammar.generate_tags()
-    print(grammar.df.head())
-    print(grammar.df.tail())
-    print(grammar.get_vectorize_data())
